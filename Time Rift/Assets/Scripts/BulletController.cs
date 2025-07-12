2f565ff51bddfc0d3b14a20dd406b7769ab0642d
Joshua Desroches <josh.des.21@gmail.com>
2025-07-12 00:23:35 -0400

    12/07/25 - FULL ENEMY NAVIGATION
    HELL YESSSSSSS

--- Time Rift/Assets/Scripts/BulletController.cs
+++ Time Rift/Assets/Scripts/BulletController.cs
@@ -4,6 +4,7 @@ public class BulletController : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Destroy things");
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
--- Time Rift/Assets/Scripts/EnemyNavigation.cs
+++ Time Rift/Assets/Scripts/EnemyNavigation.cs
@@ -1,17 +1,35 @@
-using UnityEngine;
 using Pathfinding;
 using System;
+using System.Threading;
+using UnityEngine;
 
 public class EnemyNavigation : MonoBehaviour
 {
+    [Header("Main Navigation System")]
     [SerializeField] Transform target;
     [SerializeField] float speed = 200f;
     [SerializeField] float nextPointDistance = 3f;
+    [SerializeField] float maxChaseDistance = 30f;
+    [SerializeField] float minChaseDistance = 7f;
 
+
+    [Header("Jumping System")]
     [SerializeField] float jumpForce = 5f;
     [SerializeField] Transform groundChecker;
     [SerializeField] float groundCheckDistance = 0.2f; // Adjust as needed
     [SerializeField] LayerMask groundLayer; // Set this in the Inspector
+
+    [Header("Shooting System")]
+    [SerializeField] float attackSpeed;
+    [SerializeField] string enemyType;
+
+    [SerializeField] Rigidbody2D bullet;
+    [SerializeField] Transform barrel;
+    [SerializeField] Transform muzzle;
+    [SerializeField] float bulletSpeed = 15f;
+    [SerializeField] float bulletLife = 3f;
+
+    private bool isLookingRight = true;
 
     Path path;
     int currentWaypoint = 0;
@@ -33,15 +51,69 @@ public class EnemyNavigation : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         InvokeRepeating("UpdatePath", 0f, 0.5f);
+        InvokeRepeating("AIShoot", 0f, attackSpeed);
+    }
+
+    private void LookDirection()
+    {
+        Vector3 direction = target.position - barrel.position;
+        if (isLookingRight)
+        {
+            Quaternion rotation = Quaternion.FromToRotation(barrel.right, direction);
+            barrel.rotation = rotation * barrel.rotation;
+        }
+        else if (!isLookingRight)
+        {
+            Quaternion rotation = Quaternion.FromToRotation(-barrel.right, direction);
+            barrel.rotation = rotation * barrel.rotation;
+        }
+
+        if (direction.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 2f, 1f);
+            isLookingRight = false;
+        }
+
+        else if (direction.x > 0f)
+        {
+            transform.localScale = new Vector3(1f, 2f, 1f);
+            isLookingRight = true;
+        }
     }
     
+    private void AIShoot()
+    {
+        Rigidbody2D rb = Instantiate(bullet, muzzle.position, barrel.rotation);
+
+        //AudioManager.Play("shot");
+        if (isLookingRight)
+        {
+            rb.linearVelocity = barrel.right * bulletSpeed;
+        }
+        else if (!isLookingRight)
+        {
+            rb.linearVelocity = -barrel.right * bulletSpeed;
+        }
+
+        Destroy(rb.gameObject, bulletLife);
+    }
+
     void UpdatePath()
     {
-        if (seeker.IsDone())
+        float distanceToPlayer = Vector2.Distance(rb.position, target.position);
+
+        if (distanceToPlayer <= maxChaseDistance && distanceToPlayer > minChaseDistance && seeker.IsDone())
         {
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+            path = null;
+        }
+
     }
+
     private void FixedUpdate()
     {
         if (path == null)
@@ -74,16 +146,9 @@ public class EnemyNavigation : MonoBehaviour
             }
         }
 
+        //------SHOOTING FUNCTIONALITY HERE-------//
+        LookDirection();
 
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            Debug.Log("Delete enemy");
-        }
     }
 
     private void OnPathComplete(Path p)
